853bff468f6db10cb4d030b9e0a245cdaddd030c
Julianne <julivolotao@gmail.com>
2020-02-18 12:25:53 -0300

    Index pronto

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div >
     <nuxt />
   </div>
 </template>
@@ -15,8 +15,12 @@ html {
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
+}
+
+body, html {
   height: 100%;
   width: 100%;
+  
 }
 
 *,
@@ -26,32 +30,8 @@ html {
   margin: 0;
 }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
+div:first-of-type {
+  height: 100%;
 }
 
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,124 +1,281 @@
-<template>
-  <main class="container">
+<template lang="pug">
+  main.container
 
-    <section class="left">
-      <div class="left-logo"></div>
-      <div class="left-texto"> 
-        <h1> Aprenda, 
-          <br> Invista & 
-          <br> Transforme!
-        </h1>
-      </div>
+    section.left
 
-      <div class="left-buttons">
-        <div class="button">
-          <p> Acessar conta </p>
-        </div>
-        <div class="button">
-          <p> Criar conta </p>
-        </div>
-      </div>
+      div(v-if="!logarConta && !criarConta").inicial-content
+        div.left-logo
+        div.left-texto
+          h1 
+            | Aprenda, 
+            br 
+            | Invista &
+            br
+            | Transforme!
 
-    </section>
+        div.left-buttons
 
-    <section class="right">
-    </section>
+          div( @click="logarConta = true").button
+            a Acessar conta 
+
+          div( @click="criarConta = true").button
+            a Criar conta 
+
+
+
+      div(v-if="logarConta").login-content
+
+        span( @click="logarConta = false" ).arrow-back 
+          <svg xmlns="http://www.w3.org/2000/svg" width="10.079" height="16.608" viewBox="0 0 10.079 16.608"><defs><style>.a{fill:#01c676;}</style></defs><path class="a" d="M27.76,45.321l7.391-7.391a.913.913,0,0,1,1.291,0l.862.862a.913.913,0,0,1,0,1.289l-5.858,5.885,5.858,5.885a.912.912,0,0,1,0,1.289L36.441,54a.913.913,0,0,1-1.291,0L27.76,46.612A.913.913,0,0,1,27.76,45.321Z" transform="translate(-27.492 -37.662)"/></svg>
+          span Voltar
+
+        div.left-logo
+
+        div.login-texto
+          h2 Olá
+          span Seja bem vindo novamente!
+
+        input(type="number" placeholder="Seu CPF").input
+        input(type="text" placeholder="Sua senha").input
+
+        span.esqueceu-senha Esqueceu a senha?
+
+        nuxt-link( to="/dashboard" style="text-decoration: none")
+          div( @click="").button.btnLogar
+            a Entrar
+
+        span.semconta Ainda não tenho conta: 
+          span( @click="criarConta = true, logarConta = false") cadastre-se
+
+
+      div(v-if="criarConta").cadastro-content
+
+        span( @click="logarConta = false, criarConta = false" ).arrow-back 
+          <svg xmlns="http://www.w3.org/2000/svg" width="10.079" height="16.608" viewBox="0 0 10.079 16.608"><defs><style>.a{fill:#01c676;}</style></defs><path class="a" d="M27.76,45.321l7.391-7.391a.913.913,0,0,1,1.291,0l.862.862a.913.913,0,0,1,0,1.289l-5.858,5.885,5.858,5.885a.912.912,0,0,1,0,1.289L36.441,54a.913.913,0,0,1-1.291,0L27.76,46.612A.913.913,0,0,1,27.76,45.321Z" transform="translate(-27.492 -37.662)"/></svg>
+          span Voltar
+
+        div.left-logo
+
+        div.cadastro-texto
+          h2 Olá
+          span Eu sou o MoneyBot e vi que você 
+            br
+            | está querendo ter uma conta nova.
+            br 
+            br 
+            | Infelizmente nós não estamos criando contas no momento,
+            | mas em 
+            font breve 
+            | nós poderemos conversar um pouco mais, 
+            br
+            | 
+            font até logo!
+
+
+
+    section.right
     
   </main>
 </template>
 
 <script>
-
 export default {
-  
+  data() {
+    return {
+      logarConta: false,
+      criarConta: false,
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style lang="sass" scoped>
 
-  *{
-    font-family: 'Roboto', sans-serif;
-  }
+  *
+    font-family: 'Roboto', sans-serif
+  
 
-  .container {
-    width: 100%;
-    max-width: 1024px;
-    height: 620px;
-    margin: 60px auto;
-    background-color: #F2F2F2;
-    border-radius: 5px;
-    display: flex;
-  }
+  .container
+    width: 100%
+    justify-content: space-between
+    height: 100%
+    background-color: #F2F2F2
+    border-radius: 5px
+    display: flex
+    box-sizing: border-box
 
-  .left{
-    position: relative;
-    width: 50%;
-    height: 100%;
-    padding: 50px 60px;
-    display: flex;
-    flex-direction: column;
-  }
+    .left
+      position: relative
+      padding: 60px
+      width: 35%
+      justify-content: center
+      display: flex
+      flex-direction: column
+      box-sizing: border-box
+      margin-left: auto
 
-  .left-logo{
-    width: 140px;
-    height: 140px;
-    background-image: url("../static/logoColorido.png");
-    background-size: cover;
-    background-position: center;
-    background-repeat: no-repeat;
+      input 
+        width: 270px
+        height: 50px
+        border-radius: 4px
+        border: 3px solid #01C676
+        background-color: transparent
+        outline: none
+        box-sizing: border-box
+        padding: 4px 12px
+        font-size: 1em
+        margin: 0 0 20px 30px
 
-  }
+      .left-logo
+        width: 140px
+        height: 140px
+        background-image: url("../static/logoColorido.png")
+        background-size: cover
+        background-position: center
+        background-repeat: no-repeat
 
-  .left-texto {
-    padding-left: 30px;
-    margin-top: 30px;
-    font-size: 24px;
-    line-height: 45px;
-    color: #01C676;
-  }
 
-  .left-buttons {
-    margin: 60px 0 0 30px;
-    height: 60px;
-    width: 320px;
-    position: relative;
-    display: flex;
-    justify-content: space-between;
-  }
+      .inicial-content
+        display: flex
+        flex-direction: column
+        justify-content: center
+        width: 100%
 
-  .button{
-    position: relative;
-    border: #01C676 3px solid;
-    border-radius: 5px;
-    height: 100%;
-    width: 150px;
-    display: flex;
-    text-align: center;
-    padding: 10px;
-    cursor: pointer;
-    transition: all .4s;
-  }
+        .left-texto
+          padding-left: 30px
+          margin-top: 30px
+          font-size: 24px
+          line-height: 45px
+          color: #01C676
 
-  .button:hover {
-    transform: scale(1.08);
-  }
+          h1 
+            text-align: left
 
-  .button p {
-    width: 124px;
-    line-height: 36px;
-    color: #4A4A4A;
-    font-weight: bold;
-  }
+        .left-buttons
+          margin: 60px 0 0 30px
+          height: 60px
+          width: 320px
+          position: relative
+          display: flex
+          justify-content: space-between
 
-  .right {
-    position: relative;
-    width: 50%;
-    background-image: url("../static/fundoMulher.png");
-    background-size: cover;
-    background-position: center;
-    background-repeat: no-repeat;
-    height: 100%;
-  }
-</style>>
+          
+      
+      .login-content
+        display: flex
+        flex-direction: column
+        justify-content: center
+        width: 100%
+
+        .login-texto 
+          padding-left: 30px
+          margin-bottom: 30px
+
+          h2
+            color: #01C676
+            font-size: 24px
+            margin-top: 30px
+
+          span 
+            color: #4A4A4A
+            font-size: 18px
+
+        .esqueceu-senha
+          padding-left: 30px
+          font-size: 12px
+          color: #01c576
+          margin-top: -10px
+
+        .btnLogar
+          margin: 80px 0 0 85px
+
+        .semconta
+          text-align: center
+          font-size: 12px
+          margin-left: -50px
+          margin-top: 10px
+          color: #A4A4A4
+
+          span
+            color: #01c576
+            cursor: pointer
+
+        
+      .cadastro-content
+        display: flex
+        flex-direction: column
+        justify-content: center
+        width: 100%
+
+        .cadastro-texto 
+          padding-left: 30px
+          margin-bottom: 30px
+
+          h2
+            color: #01C676
+            font-size: 24px
+            margin-top: 30px
+
+          span 
+            color: #4A4A4A
+            font-size: 18px
+
+            font 
+              color: #01C676
+          
+    
+
+    .right
+      position: relative
+      width: 55%
+      background-image: url("../static/fundooval.jpg")
+      background-size: cover
+      background-repeat: no-repeat
+      height: 100%
+
+
+
+
+    .arrow-back 
+      padding-left: 30px
+      font-size: 1em
+      color: #01C676
+      display: flex
+      margin-bottom: 20px
+      width: fit-content
+      cursor: pointer
+      transition: all .4s
+
+      span 
+        margin-left: 5px
+
+      &:hover 
+        transform: scale(1.07)
+
+    .button
+      position: relative
+      border: #01C676 3px solid
+      border-radius: 5px
+      height: 50px
+      width: 150px
+      display: flex
+      text-align: center
+      padding: 10px
+      cursor: pointer
+      transition: all .4s
+      justify-content: center
+      text-decoration: none
+
+      &:hover 
+        transform: scale(1.08)
+    
+      a
+        line-height: 24px
+        color: #4A4A4A
+        font-weight: bold
+        text-decoration: none
+      
+    
+</style>
 
 
